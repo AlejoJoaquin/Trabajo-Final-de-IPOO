906cac98713139350bf66b230e3adf3ee3deb1d1
Alejo Joaquin <alejojoaquin011@gmail.com>
2025-06-27 05:52:37 -0300

    agregamos comentarios a la base de datos

--- Persona.php
+++ Persona.php
@@ -1,12 +1,14 @@
 <?php
+//incluimos la base de datos
 include_once "BaseDeDatos.php";
 
 class Persona {
+    //declaramos los atributos de la clase
     private $documento;
     private $nombre;
     private $apellido;
     private $mensajeDeOperacion;
-    private $estadoPersona;
+    private $estadoPersona;//controlamos si la persona esta activa (true) o fue eliminada logicamente (false)
 
     public function __construct() {
         $this->documento = "";
@@ -64,6 +66,7 @@ class Persona {
                "Documento: " . $this->getDocumento() . "\n";
     }
 
+    //implementamos el metodo de insertar que inserta una nueva persona
     public function insertar() {
         $base = new BaseDeDatos();
         $respuesta = false;
@@ -84,6 +87,7 @@ class Persona {
         return $respuesta;
     }
 
+    //implementamos el metodo buscar que busca a una persona en la base de datos
     public function buscar($documento) {
         $base = new BaseDeDatos();
         $respuesta = false;
@@ -91,6 +95,7 @@ class Persona {
         if ($base->Iniciar()) {
             if ($base->Ejecutar($consulta)) {
                 if ($fila = $base->Registro()) {
+                    //si la encuentra, seteamos los valores del objeto
                     $this->setDocumento($fila["documento"]);
                     $this->setNombre($fila["nombre"]);
                     $this->setApellido($fila["apellido"]);
@@ -105,6 +110,7 @@ class Persona {
         return $respuesta;
     }
 
+    //implementamos el metodo modificar de una persona existente
     public function modificar() {
         $base = new BaseDeDatos();
         $respuesta = false;
@@ -121,6 +127,7 @@ class Persona {
         return $respuesta;
     }
 
+    //implementamos el metodo de eliminar, que elimina a una persona(no la borra de la base de datos, sino que la marca como inactiva)
     public function eliminar() {
         $base = new BaseDeDatos();
         $respuesta = false;
@@ -156,18 +163,21 @@ class Persona {
         return $respuesta;
     }
 
-
+    //listara a todas las persona activas, opcionalmente con una condicion
     public function listar($condicion = "") {
         $arreglo = [];
         $base = new BaseDeDatos();
         $consulta = "SELECT * FROM persona WHERE estadoPersona = TRUE";
+        //aca si se pasa una condicion adicional, se le agregara a la consulta
         if ($condicion != "") {
             $consulta .= " AND " . $condicion;
         }
+        //ordenamos por apellido
         $consulta .= " ORDER BY apellido";
 
         if ($base->Iniciar()) {
             if ($base->Ejecutar($consulta)) {
+                //por cada fila encontrada, creamos el objeto persona y lo agregamos al arreglo
                 while ($fila = $base->Registro()) {
                     $obj = new Persona();
                     $obj->setDocumento($fila["documento"]);
